2c48e3ba0d89e2df43646d803670e0353cb8e208
Neeraj Verma <97516757+neeraj9649@users.noreply.github.com>
2022-12-27 14:26:53 +0530

    Add files via upload

---
+++ bkchodi.cpp
@@ -0,0 +1,24 @@
+// #include <bits/stdc++.h>
+#include <string.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"enter your name :- ";
+    cin>>name;
+    // if(name=="deepak"){
+    //     cout<<name<<" you are gandu";
+    //     cout<<endl<<"rhne do beta tumse no ho payega";
+    //     // break;
+    // }
+    // else{
+    //     cout<<"welcome "<<name<< " to the coding world"<<endl;
+    //     goto start;
+    // }
+    
+    return 0;
+}
+
+// output  cout<<" ";
+// Input   cin>> variable name;
